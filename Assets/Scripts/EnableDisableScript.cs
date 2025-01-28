1968a18e11e96e17b21d0ed61feecea265b7e574
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-01-28 16:50:53 -0500

    Practicing with enabled and SetActive.

---
+++ Assets/Scripts/EnableDisableScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableScript : MonoBehaviour
+{
+
+    public SpriteRenderer circleSprite;
+    public EnableDisableScript script;
+    public GameObject badUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0)) { 
+        
+            circleSprite.enabled = false;
+            badUI.SetActive(false);
+        
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            circleSprite.enabled = true;
+            badUI.SetActive(true);
+
+        }
+
+    }
+}
