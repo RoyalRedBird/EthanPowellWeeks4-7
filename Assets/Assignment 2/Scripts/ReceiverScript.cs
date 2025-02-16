3312c66b262173984f18e91d1a13df2606eb0ec2
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-02-15 22:01:11 -0500

    Added ReceiverScript, receivers are now selectable. Fixed typo in the Mag selector.

---
+++ Assets/Assignment 2/Scripts/ReceiverScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiverScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject stalkerReceiver;
+    [SerializeField] GameObject autoReceiver;
+    [SerializeField] GameObject rifleReceiver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwapReceiver(int select)
+    {
+
+        if (select == 0)
+        {
+
+            stalkerReceiver.SetActive(false);
+            autoReceiver.SetActive(false);
+            rifleReceiver.SetActive(true);
+
+        }
+
+        if (select == 1)
+        {
+
+            stalkerReceiver.SetActive(true);
+            autoReceiver.SetActive(false);
+            rifleReceiver.SetActive(false);
+
+        }
+
+        if (select == 2)
+        {
+
+            stalkerReceiver.SetActive(false);
+            autoReceiver.SetActive(true);
+            rifleReceiver.SetActive(false);
+
+        }
+
+    }
+
+}
