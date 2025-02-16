515f0e7a62b888cfa71afbc66b03df68e46eec60
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2025-02-15 20:53:42 -0500

    Constructed a nightmare matrix of Vector2's for the dynamic UI elements. God help me.

--- Assets/Assignment 2/Scripts/LoreBoxScript.cs
+++ Assets/Assignment 2/Scripts/LoreBoxScript.cs
@@ -14,11 +14,66 @@ public class LoreBoxScript : MonoBehaviour
     int buttonFullPosX = 1225;
     int buttonCollapsedPosX = 30;
 
+    Vector2 windowExpandedPos;
+    Vector2 windowCollapsedPos;
+    Vector2 windowHiddenPos;
+
+    Vector2 expandButtonExpandedPos;
+    Vector2 expandbuttonCollapsedPos;
+    Vector2 expandbuttonHiddenPos;
+
+    Vector2 collapseButtonExpandedPos;
+    Vector2 collapsebuttonCollapsedPos;
+    Vector2 collapsebuttonHiddenPos;
+
+    [SerializeField] AnimationCurve SmoothInCurve;
+    float SmoothInTime;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Vector2 windowExpandPos = LoreBoxSize.position;
+        Vector2 windowCollapsePos = LoreBoxSize.position;
+        Vector2 windowObscurePos = LoreBoxSize.position;
+
+        windowExpandPos.x = Screen.width / 2;
+        windowCollapsePos.x = 150;
+        windowObscurePos.x = -400;
+
+        windowExpandedPos = windowExpandPos;
+        windowCollapsedPos = windowCollapsePos;
+        windowHiddenPos = windowObscurePos;
+
+        //
+
+        Vector2 expBtnExpPos = ExpandButton.position;
+        Vector2 expBtnColPos = ExpandButton.position;
+        Vector2 expBtnHdnPos = ExpandButton.position;
+
+        expBtnExpPos.x = buttonFullPosX;
+        expBtnColPos.x = buttonPartialPosX;
+        expBtnHdnPos.x = buttonCollapsedPosX;
+
+        expandButtonExpandedPos = expBtnExpPos;
+        expandbuttonCollapsedPos = expBtnColPos;
+        expandbuttonHiddenPos = expBtnHdnPos;
+
+        //
+
+        Vector2 colBtnExpPos = CollapseButton.position;
+        Vector2 colBtnColPos = CollapseButton.position;
+        Vector2 colBtnHdnPos = CollapseButton.position;
+
+        colBtnExpPos.x = buttonFullPosX;
+        colBtnColPos.x = buttonPartialPosX;
+        colBtnHdnPos.x = buttonCollapsedPosX;
+
+        collapseButtonExpandedPos = colBtnExpPos;
+        collapsebuttonCollapsedPos = colBtnColPos;
+        collapsebuttonHiddenPos = colBtnHdnPos;
+
     }
 
     // Update is called once per frame
